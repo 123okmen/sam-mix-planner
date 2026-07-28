74edb67453fa33031683740e546156e24c6eada9
123okmen <123okmen@users.noreply.github.com>
2026-07-28 18:11:46 +0700

    Update logo with user provided image

---
+++ copyUserLogo.cjs
@@ -0,0 +1,6 @@
+const fs = require('fs');
+fs.copyFileSync(
+  'c:\\Users\\pc\\Desktop\\Plan quán nước\\logo.jpg',
+  'c:\\Users\\pc\\Desktop\\Plan quán nước\\sam-mix-planner\\public\\logo_v2.jpg'
+);
+console.log('Copied successfully');
